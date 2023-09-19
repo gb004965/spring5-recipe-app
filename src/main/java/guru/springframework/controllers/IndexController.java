8e58481cff8f166e838f407da3dc1e9f3a5d02dc
Rory O'Connor <gonzooo@gmail.com>
2023-09-19 10:21:17 +1000

    Simple change to trigger circleci build

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by jt on 6/1/2017.
+ * Created by jt on 6/1/2017.2
  */
 @Slf4j
 @Controller
